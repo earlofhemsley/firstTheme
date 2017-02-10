f59eee38cd837a00da88bf44da7f4522878a3620
Landon Hemsley <lhemsley@egovmt.com>
2017-02-10 07:34:42 -0700

    Add margin to nav, remove ready from js since it's now at bottom

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
     $('.lmhcustom-nav-toggle').on('click', function(){
         if($(this).children(':first-child').html() == "Show Menu"){
             $(this).children(':first-child').html("Hide Menu");
@@ -8,4 +7,3 @@ $(document).ready(function(){
         
         }
     });
-});
