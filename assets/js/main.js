106842798758dbfa8b615c4ff18fc43ff3319c7a
Landon Hemsley <lhemsley@egovmt.com>
2017-02-16 07:17:48 -0700

    really finish homepage header section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,36 @@
-$('.lmhcustom-nav-toggle').on('click', function(){
-    if($(this).children(':first-child').html() == "Show Menu"){
-        $(this).children(':first-child').html("Hide Menu");
+var resize = function(){
+    var sections = document.getElementsByClassName("home-section");
+    for(var i = 0; i<sections.length; i++){
+        sections[i].style.height = window.innerHeight + "px";
+    }
+    if(window.innerHeight > 440){
+        document.getElementById("home-leadbox").style.top = (0.25 * window.innerHeight) + "px";
     }
     else{
-        $(this).children(':first-child').html("Show Menu");
-    
+        document.getElementById("home-leadbox").style.top =  "10px";
     }
-});
+}
+
+
 
 $(document).ready(function(){
+    resize();
     var wrapper = $("#wrapper:hidden");
     if(wrapper != null){
         wrapper.delay(200).fadeIn();
     }
+
+    $('.lmhcustom-nav-toggle').on('click', function(){
+        if($(this).children(':first-child').html() == "Show Menu"){
+            $(this).children(':first-child').html("Hide Menu");
+        }
+        else{
+            $(this).children(':first-child').html("Show Menu");
+        
+        }
+    });
+    
+    window.onresize = resize;
+
+
 });
