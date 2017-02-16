1aeb9eb92430b0085ff59680f1db71f342852638
Landon Hemsley <lhemsley@egovmt.com>
2017-02-16 07:13:56 -0700

    Finished header section of home page

--- template-parts/header/home-header.php
+++ template-parts/header/home-header.php
@@ -1,16 +1,16 @@
 <!-- home-header -->
     <div id="" class="home-section">
-        <div class="home-leadbox">
-            <div class="text-center"><?php the_custom_logo(); ?></div>
-            <h1 class="text-center"><?php bloginfo("name"); ?></h1>
-            <h2 class="text-center"><?php bloginfo("description"); ?></h2>
+        <div id="home-leadbox">
+            <div class="text-center slightly-padded"><?php the_custom_logo(); ?></div>
+            <h1 class="text-center slightly-padded"><?php bloginfo("name"); ?></h1>
+            <h2 class="text-center slightly-padded"><?php bloginfo("description"); ?></h2>
 		</div>
         <?php 
             wp_nav_menu( array( 
                 'theme_location' => 'root',
                 'menu_class' => '',
                 'container' => 'div',
-                'container_class' => '',
+                'container_class' => 'lmh-home-root-menu-container',
                 'container_id' => ''
             )); 
         ?>
