640b9ab019f009f42879e86a64d21a37985b005d
Landon Hemsley <lhemsley@egovmt.com>
2017-02-24 07:36:36 -0700

    order home page posts by the same order that they are in the menu

--- template-parts/body/home-body.php
+++ template-parts/body/home-body.php
@@ -8,10 +8,10 @@
         foreach($root_menu_items as $item){
             $page_ids[] = get_post_meta( $item->ID, '_menu_item_object_id', true);
         }
-        //TODO: ORDER IN THE SAME ORDER THAT THEY ARE IN THE MENU
         $query = new WP_Query( array(
             'post_type' => 'page',
-            'post__in' => $page_ids 
+            'post__in' => $page_ids,
+            'orderby' => 'post__in', 
         ) );
 
         while($query->have_posts()):
