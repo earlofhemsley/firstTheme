0cf0d4281f56a13e2968d4edbc8c2c0ff9f617a2
Landon Hemsley <lhemsley@egovmt.com>
2017-02-24 07:29:40 -0700

    pull content onto homepage based on root menu items

--- template-parts/body/home-body.php
+++ template-parts/body/home-body.php
@@ -1,19 +1,28 @@
 <?php 
-    
-    if( $locations = get_nav_menu_locations() && isset ($locations["root"]) ):
+    $locations = get_nav_menu_locations();
+    //var_dump($locations);
+    //echo '<br />' . isset($locations["root"]).'<br />' ;
+    if( isset ($locations["root"]) ):
         $root_menu_items = wp_get_nav_menu_items($locations["root"]);
+        //var_dump($root_menu_items); echo '<br />';
         foreach($root_menu_items as $item){
-            $page_ids[] = $item->ID;
+            $page_ids[] = get_post_meta( $item->ID, '_menu_item_object_id', true);
         }
         $query = new WP_Query( array(
             'post_type' => 'page',
             'post__in' => $page_ids 
         ) );
+
+        //var_dump($query); 
         while($query->have_posts()):
             $query->the_post();
+?>
+    <h2><?php the_title(); ?></h2>
+    <div><?php the_content(); ?></div>
+<?php
             //write some html
             $query->next();
-        endif;
+        endwhile;
 
     endif;
 ?>
