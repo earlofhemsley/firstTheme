2f484b14b90b4434be1e54c5d5c6f31fd150bc4d
Landon Hemsley <lhemsley@egovmt.com>
2017-06-09 07:41:40 -0600

    site search 2: site search done - meant to be used in widgets

---
+++ template-parts/search/site.php
@@ -0,0 +1,8 @@
+<form action="<?php get_home_url(); ?>" method="get" role="search">
+    <div class="form-horizontal lmh-site-search">
+        <div class="form-group">
+            <input type="text" class="form-control" name="s" />
+            <button class="btn btn-default" type="submit">Submit</button>
+        </div>
+    </div>
+</form>
--- template-parts/sidebar/site-sidebar.php
+++ template-parts/sidebar/site-sidebar.php
@@ -1,10 +1,4 @@
 <div class="site-sidebar">
-    <div class="site-sidebar-section">
-        <h2><?php _e('Blog archives'); ?></h2>
-        <ul class="">
-            <?php wp_get_archives('type=monthly'); ?>
-        </ul>
-    </div>
 <?php 
     $names = array('lmh-primary','lmh-secondary','lmh-tertiary');
     foreach($names as $name):
