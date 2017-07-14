0cebc28e83d39ffe1355ddfdd2822001a2b8aa49
Landon Hemsley <lhemsley@egovmt.com>
2017-07-13 20:30:00 -0600

    pre- and append the style tags to the content of the walker

--- assets/classes/Elegance_Comment_Walker.class.php
+++ assets/classes/Elegance_Comment_Walker.class.php
@@ -1,45 +1,14 @@
 <?php
 class Elegance_Comment_Walker extends Walker_Comment{
 
-    private function prepare_values($comment, $depth, $args){
-        $props = array();
-        $props['subtag_open'] = '';
-        $props['subtag_close'] = '';
-        $props['tag'] = 'div';
-        $add_below = 'elegance-comment';
+    public function paged_walk($elements, $max_depth, $page_num, $per_page){
+        $args = array_slice(func_get_args(), 4);
+        $args = $args[0];
 
-        if('div' != $args['style'] || 'html5' == $args['format']){
-            $add_below = 'elegance-comment-subdiv'; 
+        $output = parent::paged_walk($elements, $max_depth, $page_num, $per_page, $args);
 
-            if('div' != $args['style']) $props['tag'] = 'li';
-            $subtag = ('html5' == $args['format']) ? 'article' : 'div';
-            $props['subtag_open'] = '<'.$subtag.' id="elegance-comment-subdiv-'.$comment->comment_ID.'">';
-            $props['subtag_close'] = '</'.$subtag.'>';
-        }
-
-        $props['wrapper_class'] = get_comment_class( $this->has_children ? 'elegance-comment-parent elegance-comment' : 'elegance-comment', $comment );
-        $props['comment_id'] = 'elegance-comment-' . $comment->comment_ID;
-        $props['avatar'] = get_avatar($comment, $args['avatar_size']);
-        $props['author_meta'] = sprintf('<strong>%s</strong> <span class="elegance-comment-time-posted">%s</span>',
-            get_comment_author_link($comment->comment_ID),
-            sprintf('%1$s %2$s', get_comment_date('', $comment->comment_ID), get_comment_time())
-        );
-        $props['comment_text'] = ('0' == $comment->comment_approved) ? 'This comment is awaiting moderation' : get_comment_text($comment, array_merge($args, array('add_below' => $add_below, 'depth' => $depth)));
-
-        $props['reply_link'] = get_comment_reply_link(array(
-            'add_below' => $add_below,
-            'login_text' => 'Log in',
-            'max_depth' => $args['max_depth'],
-            'depth' => $depth,
-            'before' => '<div class="elegance-comment-reply">',
-            'after' => '</div><!-- elegance-comment-reply -->'
-        ));
-
-        $temp_edit_link =  get_edit_comment_link($comment);
-        $props['edit_link'] = ($temp_edit_link) ? 
-                '<span class="alignright elegance-comment edit">'.$temp_edit_link.'</span>' : '';
-
-        return $props;
+        if($args['style'] != 'div') return "<{$args['style']} class='elegance-comment-top-level'>".$output."</{$args['style']}>";
+        else return $output;
     }
 
     protected function comment($comment, $depth, $args){
@@ -92,5 +61,46 @@ EOT;
             </div>
 <?php
         }
+    
+    private function prepare_values($comment, $depth, $args){
+        $props = array();
+        $props['subtag_open'] = '';
+        $props['subtag_close'] = '';
+        $props['tag'] = 'div';
+        $add_below = 'elegance-comment';
+
+        if('div' != $args['style'] || 'html5' == $args['format']){
+            $add_below = 'elegance-comment-subdiv'; 
+
+            if('div' != $args['style']) $props['tag'] = 'li';
+            $subtag = ('html5' == $args['format']) ? 'article' : 'div';
+            $props['subtag_open'] = '<'.$subtag.' id="elegance-comment-subdiv-'.$comment->comment_ID.'">';
+            $props['subtag_close'] = '</'.$subtag.'>';
+        }
+
+        $props['wrapper_class'] = get_comment_class( $this->has_children ? 'elegance-comment-parent elegance-comment' : 'elegance-comment', $comment );
+        $props['comment_id'] = 'elegance-comment-' . $comment->comment_ID;
+        $props['avatar'] = get_avatar($comment, $args['avatar_size']);
+        $props['author_meta'] = sprintf('<strong>%s</strong> <span class="elegance-comment-time-posted">%s</span>',
+            get_comment_author_link($comment->comment_ID),
+            sprintf('%1$s %2$s', get_comment_date('', $comment->comment_ID), get_comment_time())
+        );
+        $props['comment_text'] = ('0' == $comment->comment_approved) ? 'This comment is awaiting moderation' : get_comment_text($comment, array_merge($args, array('add_below' => $add_below, 'depth' => $depth)));
+
+        $props['reply_link'] = get_comment_reply_link(array(
+            'add_below' => $add_below,
+            'login_text' => 'Log in',
+            'max_depth' => $args['max_depth'],
+            'depth' => $depth,
+            'before' => '<div class="elegance-comment-reply">',
+            'after' => '</div><!-- elegance-comment-reply -->'
+        ));
+
+        $temp_edit_link =  get_edit_comment_link($comment);
+        $props['edit_link'] = ($temp_edit_link) ? 
+                '<span class="alignright elegance-comment edit">'.$temp_edit_link.'</span>' : '';
+
+        return $props;
+    }
 
 }
