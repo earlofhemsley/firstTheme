703e8e33391debe4ebc02992d9e8ca396ad8fddc
Landon Hemsley <lhemsley@egovmt.com>
2017-08-30 06:59:45 -0600

    i18n of footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 	<div id="footer">
-        <p class="copyright-text text-center">Copyright &copy; <?php echo date("Y"); ?></p>
+        <p class="copyright-text text-center"><?php _e('Copyright &copy','elegance');?> <?php echo date("Y"); ?></p>
 	</div>
 </div>
 <?php wp_footer(); ?>
